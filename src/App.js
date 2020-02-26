31813a316141357c2a14a105717abe63f165bdc4
Qlif <baal72alex@gmail.com>
2020-02-26 12:22:39 +0200

    Create base for testing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Footer(){
+  return (
+    <footer>Im a footer </footer>
+  );
+}
+
+function Body(){
+  return (
+    <body>Body will by heare</body>
+  )
+}
+
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,8 +32,18 @@ function App() {
           Learn React
         </a>
       </header>
+        <body className="App-body">
+          <p>
+            <Body/>
+          </p>
+        </body>
+      <footer className="App-footer">
+            <Footer/>
+      </footer>
     </div>
+
   );
+
 }
 
 export default App;
